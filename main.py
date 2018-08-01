fd325677839a6c79c880f1bbd4806583a2ba288f
Li, Louis (L&W, Dutton Park) <Wenjin.Li@csiro.au>
2018-08-01 16:43:30 +1000

    faster scan

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
-
-with open('ScanResults/250720181645.txt', 'r') as f:
+with open('ScanResults/31072018.txt', 'r') as f:
     rows = f.read()
     text_lines = rows.split('#################')
     for i, row in enumerate(text_lines):
         if 'Apache' in str(row):
             host = text_lines[i - 1]
             # host = host.strip('Host: ')
-            print(host)
+            #print(host)
     print(len(host))
 
 
--- test.py
+++
@@ -1,6 +0,0 @@
-with open('ScanResults/250720181645.txt', 'r') as fileReadObj:
-    rows = fileReadObj.read()
-    text_lines = rows.split('#################')
-    for i, row in enumerate(text_lines):
-        if 'Apache' in str(row):
-            print(text_lines[i - 1])
