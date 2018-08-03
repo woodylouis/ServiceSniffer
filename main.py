ddf31562006f16fc38be67debee5a88cc7dce14d
li252 <Wenjin.Li@csiro.au>
2018-08-03 23:46:37 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+with open('31072018.txt', 'r') as f:
+    rows = f.read()
+    text_lines = rows.split('#################')
+    for i, row in enumerate(text_lines):
+        if 'Apache' in str(row):
+            host = text_lines[i - 1]
+            # host = host.strip('Host: ')
+            #print(host)
+    print(len(host))
+
+
+
+
