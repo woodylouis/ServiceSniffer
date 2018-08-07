e010acb8ff1a5afb8c41283cf7a177920aacf1b3
Li, Louis (L&W, Dutton Park) <Wenjin.Li@csiro.au>
2018-08-07 14:55:06 +1000

    implement Python HTTP Request

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
+import requests
+import sys
 
-with open('file.txt', 'r') as f:
+with open('C:/Users/LI252/Desktop/ScanResults/02082018_ScanningRawData.txt', 'r') as f:
     rows = f.read()
     text_lines = rows.split('#################')
+    httpHostList = []
     for i, row in enumerate(text_lines):
-        if 'Apache' in str(row):
-            host = text_lines[i - 1]
-            # host = host.strip('Host: ')
-            #print(host)
-    print(len(host))
+        if 'http' in str(row):
+            hosts = text_lines[i - 1].strip("Host: ")
+            httpHostList.append(hosts)
+    # print(httpHostList)
+    # print(len(httpHostList))
+    for httpHost in httpHostList:
+        urls = 'http://' + httpHost + '/thredds/catalog.html'
+        # print(urls)
+        try:
+            r = requests.get(urls, timeout=1)
+            r.raise_for_status()
+            print("Http Connected", httpHost, r)
+
+
+        except requests.exceptions.HTTPError as errh:
+
+            print("Http Error:", errh)
+
+        except requests.exceptions.ConnectionError as errc:
+
+            print("Connection Error for ", httpHost)
+
+        except requests.exceptions.Timeout as errt:
+
+            print("Timeout Error:", errt)
+
+        except requests.exceptions.RequestException as err:
+
+            print("OOps: Something Else", err)
+
+
+
 
 
 
