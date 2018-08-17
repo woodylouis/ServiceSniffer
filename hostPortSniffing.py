d9b50ca37258582fbafe4c102779d740c7699d4a
Li, Louis (L&W, Dutton Park) <Wenjin.Li@csiro.au>
2018-08-17 14:46:30 +1000

    combined code

--- hostPortSniffing.py
+++ hostPortSniffing.py
@@ -2,87 +2,99 @@ import nmap
 import sys
 import time
 import os
+import datetime
 
-addresses = []
-address = []
-try:
-    with open(input("Please enter the path for the files that contains address: "), "r") as f:
-
-        li = [i.strip().split() for i in f]
-        addresses = [item for sublist in li for item in sublist]
-        for address in addresses:
-            print(''.join(address))
-except:
-    print("Please enter a valid file path")
+date = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
+reportName = str(date) + "_rawData" + ".txt"
+scanReport = open(reportName, "a")
 
 class PingHosts:
-    ## network initialisation
+
+    """
+    network initialisation
+    """
     def __init__(self, networkPrefix):
         self.networkPrefix = networkPrefix
-    ## Perform simple ping and create a list of online hosts
+    ##
 
     def nmapPingScan(self):
         t1 = time.time()
         nm = nmap.PortScanner()
         activeHosts = []
+        """
+        Perform simple ping and create a list of online hosts
+        This is acceptable scanning speed and realiability for bunch of /16 network
+        """
 
-        #This is acceptable scanning speed and realiability for bunch of /16 network
-        pingScanRawResult = nm.scan(hosts=self.networkPrefix, arguments='--min-hostgroup=5000 --max-hostgroup=100000 --min-parallelism=100 --max-parallelism=200 --host-timeout=2s -T5 -n -sP')
-        print(pingScanRawResult)
-        for result in pingScanRawResult['scan'].values():
-            print(result)
+        raw_result = nm.scan(hosts=self.networkPrefix, arguments='--min-hostgroup=5000 --max-hostgroup=100000 --min-parallelism=100 --max-parallelism=200 --host-timeout=2s -T5 -n -sP')
+        #print(pingScanRawResult)
+        for result in raw_result['scan'].values():
+            #print(result)
             if result['status']['state'] == 'up':
                 activeHosts.append(result['addresses']['ipv4'])
-        print("There are " + str(len(activeHosts)) + " active hosts online. The hosts are: \n" + '\n'.join('{}: {}'.format(*k) for k in enumerate(activeHosts, start=1)) + "\n")
-        ## 1-1024 popular port
-        ## 1194 openVPN
-        ## 1433 Microsoft SQL Server
-        ## 1434 Microsoft SQL Monitor
-        ## 2483-2484 Oracle database
-        ## 3306 MySQL database service
-        ## 4333 mSQL
-        ## 5432 PostgreSQL database
-        ## 8080 HTTP Proxy such as Apache
-        ## 27017 Mongo database
+        totalActiveHosts = ("There are " + str(len(activeHosts)) + " active hosts online. The hosts are: \n" + '\n'.join('{}: {}'.format(*k) for k in enumerate(activeHosts, start=1)) + "\n")
+        scanReport.write(totalActiveHosts)
+        """
+        1-1024 popular port
+        1194 openVPN
+        1433 Microsoft SQL Server
+        1434 Microsoft SQL Monitor
+        2483-2484 Oracle database
+        3306 MySQL database service
+        4333 mSQL
+        5432 PostgreSQL database
+        8080 HTTP Proxy such as Apache
+        27017 Mongo database
+        """
         for host in activeHosts:
             fullScanRawResults = nm.scan(hosts=host, arguments='--min-hostgroup=5000 --max-hostgroup=100000 --min-parallelism=100 --max-parallelism=200 --host-timeout=2s -T5 -n -v', ports="1-1024, 1433-1434, 2483-2484, 800, 3306, 4333, 5432, 5000, 8080, 9000, 8433, 27017, 50000")
             #print(fullScanRawResults)
             for host, result in fullScanRawResults['scan'].items():
                 if result['status']['state'] == 'up':
-                    print('#' * 17 + 'Host: ' + host + '#' * 17)
+                    hostDecoration = ('#' * 17 + 'Host: ' + host + '#' * 17 + "\n")
+                    scanReport.write(hostDecoration)
                     idno = 1
                     try:
                         for port in result['tcp']:
                             try:
-                                print('-' * 17 + 'TCP Service Detail' + '[' + str(idno) + ']' + '-' * 17)
+                                id = ('-' * 17 + 'TCP Service Detail' + '[' + str(idno) + ']' + '-' * 17  + "\n")
+                                scanReport.write(id)
                                 idno = idno + 1
-                                print('TCP Port Number: ' + str(port))
+                                portNum = ('TCP Port Number: ' + str(port)  + "\n")
+                                scanReport.write(portNum)
                                 try:
-                                    print('Status:' + result['tcp'][port]['state'])
+                                    portStatus = ('Status:' + result['tcp'][port]['state'] + "\n")
+                                    scanReport.write(portStatus)
                                 except:
                                     pass
                                 try:
-                                    print('reason:' + result['tcp'][port]['reason'])
+                                    reason = ('reason:' + result['tcp'][port]['reason'] + "\n")
+                                    scanReport.write(reason)
                                 except:
                                     pass
                                 try:
-                                    print('Additional Info' + result['tcp'][port]['extrainfo'])
+                                    info = ('Additional Info' + result['tcp'][port]['extrainfo'] + "\n")
+                                    scanReport.write(info)
                                 except:
                                     pass
                                 try:
-                                    print('Service: ' + result['tcp'][port]['name'])
+                                    service = ('Service: ' + result['tcp'][port]['name'] + "\n")
+                                    scanReport.write(service)
                                 except:
                                     pass
                                 try:
-                                    print('version: ' + result['tcp']['version'])
+                                    version = ('version: ' + result['tcp']['version'] + "\n")
+                                    scanReport.write(version)
                                 except:
                                     pass
                                 try:
-                                    print('product: ' + result['tcp'][port]['product'])
+                                    product = ('product: ' + result['tcp'][port]['product'] + "\n")
+                                    scanReport.write(product)
                                 except:
                                     pass
                                 try:
-                                    print('Script: ' + result['tcp'][port]['script'])
+                                    script = ('Script: ' + result['tcp'][port]['script'] + "\n")
+                                    scanReport.write(script)
                                 except:
                                     pass
                             except:
@@ -90,14 +102,25 @@ class PingHosts:
                     except:
                         pass
 
-                    print('-' * 20 + 'IP address detail' + '-' * 20)
                     try:
-                        print('IP Address: ' + result['addresses']['ipv4'])
-                        print('MAC Address: ' + result['addresses']['mac'])
+                        ipAddr = ('IP Address: ' + result['addresses']['ipv4'] + "\n")
+                        macAddr = ('MAC Address: ' + result['addresses']['mac'] + "\n")
+                        scanReport.write(ipAddr)
+                        scanReport.write(macAddr)
                     except:
                         pass
         t2 = time.time()
-        print("-" * 17 + "Time Used" + '-' * 17 + "\n" + str("Used %.2f"%(t2 - t1) + " seconds"))
+        timeUsed = ("-" * 17 + "Time Used" + '-' * 17 + "\n" + str("Used %.2f"%(t2 - t1) + " seconds") + "\n")
+        scanReport.write(timeUsed)
+        print("\n" + "#" * 25 + "\n" + "##Scan Report Created!!##" + "\n" + "#" * 25 + "\n")
 
-result = PingHosts(address)
-result.nmapPingScan()
+try:
+    f = open(input("Please enter the path for the files that contains address: "), "r")
+    network = f.read()
+except:
+    print("Please enter a valid file path")
+
+    #This is acceptable scanning speed and realiability for bunch of /16 network
+
+test1 = PingHosts(network)
+test1.nmapPingScan()
--- httpRequestQuery.py
+++ httpRequestQuery.py
@@ -1,73 +1,79 @@
 import requests
 import time
+import hostPortSniffing
+import os
+from pathlib import Path, PureWindowsPath
 
-with open('C:/Users/LI252/Desktop/ScanResults/02082018_ScanningRawData.txt', 'r') as f:
-    rows = f.read()
-    text_lines = rows.split('#################')
-    httpHostList = []
-    threddsCandidateHostList = []
-    hostStatusDict = {}
-    noThreddsInstalledHostList = []
-    redirectToOtherURL = []
-    unknownError = []
-    t1 = time.time()
-    for i, row in enumerate(text_lines):
-        if 'http' in str(row):
-            hosts = text_lines[i - 1].strip("Host: ")
-            httpHostList.append(hosts)
+file = PureWindowsPath("C:\\Users\\LI252\\PycharmProjects\\servicesniffer\\" + hostPortSniffing.reportName)
+reportPath = Path(file)
 
-    for httpHost in httpHostList:
-        urls = 'http://' + httpHost + '/thredds/catalog.html'
+f = open(reportPath, 'rt')
+rows = f.read()
+print(reportPath)
+text_lines = rows.split('#################')
+httpHostList = []
+threddsCandidateHostList = []
+hostStatusDict = {}
+noThreddsInstalledHostList = []
+redirectToOtherURL = []
+unknownError = []
+t1 = time.time()
+for i, row in enumerate(text_lines):
+    if 'http' in str(row):
+        hosts = text_lines[i - 1].strip("Host: ")
+        httpHostList.append(hosts)
 
-        try:
-            r = requests.get(urls, timeout=0.5, allow_redirects=False)
-        # print(httpHost, r.status_code)
-        except requests.exceptions.HTTPError:
-            """An HTTP error occurred."""
-            pass
-        except requests.exceptions.ConnectionError:
-            """A Connection error occurred."""
-            pass
-        except requests.exceptions.Timeout:
-            """
-            The request timed out while trying to connect to the remote server.
+for httpHost in httpHostList:
+    urls = 'http://' + httpHost + '/thredds/catalog.html'
 
-            Requests that produced this error are safe to retry.
-            """
-            pass
-        except requests.exceptions.RequestException:
-            """
-            There was an ambiguous exception that occurred while handling your request.
-            """
-            pass
-        else:
-            if httpHost not in hostStatusDict:
-                hostStatusDict[httpHost] = str(r.status_code)
-    print("###There are", len(hostStatusDict), "host\n", hostStatusDict, "all hosts status Dict is above ###\n\n")
+    try:
+        r = requests.get(urls, timeout=0.5, allow_redirects=False)
+    # print(httpHost, r.status_code)
+    except requests.exceptions.HTTPError:
+        """An HTTP error occurred."""
+        pass
+    except requests.exceptions.ConnectionError:
+        """A Connection error occurred."""
+        pass
+    except requests.exceptions.Timeout:
+        """
+        The request timed out while trying to connect to the remote server.
 
-    for host, status in hostStatusDict.items():
-        if status == '404':
-            noThreddsInstalledHostList.append(host)
-            #print("no thredds installed in these hosts\n\n", noThreddsInstalledHostList)
-        elif status == '302':
-            # redirect to CISCO gateway login page or link has been moved permanently
-            redirectToOtherURL.append(host)
-            #print("These host redirect to other page or require to login\n\n", redirectToOtherURL)
-        elif status == '200':
-            threddsCandidateHostList.append(host)
-            #print("These host may have thredd installed\n\n", threddsCandidateHostList)
-        else:
-            unknownError.append(host)
+        Requests that produced this error are safe to retry.
+        """
+        pass
+    except requests.exceptions.RequestException:
+        """
+        There was an ambiguous exception that occurred while handling your request.
+        """
+        pass
+    else:
+        if httpHost not in hostStatusDict:
+            hostStatusDict[httpHost] = str(r.status_code)
+print("###There are", len(hostStatusDict), "host\n", hostStatusDict, "all hosts status Dict is above ###\n\n")
 
-    # for theHost in noThreddsInstalledHostList:
+for host, status in hostStatusDict.items():
+    if status == '404':
+        noThreddsInstalledHostList.append(host)
+        #print("no thredds installed in these hosts\n\n", noThreddsInstalledHostList)
+    elif status == '302':
+        # redirect to CISCO gateway login page or link has been moved permanently
+        redirectToOtherURL.append(host)
+        #print("These host redirect to other page or require to login\n\n", redirectToOtherURL)
+    elif status == '200':
+        threddsCandidateHostList.append(host)
+        #print("These host may have thredd installed\n\n", threddsCandidateHostList)
+    else:
+        unknownError.append(host)
 
-    print("There are", len(redirectToOtherURL), "hosts redirect to firewall login page\n", redirectToOtherURL, '\n')
-    print("There are", len(threddsCandidateHostList), "hosts may have Thredds installed\n", threddsCandidateHostList, '\n')
-    print("There are",  len(noThreddsInstalledHostList), "hosts have no Thredds installed in these hosts\n", noThreddsInstalledHostList, '\n')
-    print("There are", len(unknownError), "unknown error hosts\n", unknownError, '\n')
+# for theHost in noThreddsInstalledHostList:
 
-    t2 = time.time()
-    print("-" * 17 + "Time Used" + '-' * 17 + "\n" + str("Used %.2f" % (t2 - t1) + " seconds"))
+print("There are", len(redirectToOtherURL), "hosts redirect to firewall login page\n", redirectToOtherURL, '\n')
+print("There are", len(threddsCandidateHostList), "hosts may have Thredds installed\n", threddsCandidateHostList, '\n')
+print("There are",  len(noThreddsInstalledHostList), "hosts have no Thredds installed in these hosts\n", noThreddsInstalledHostList, '\n')
+print("There are", len(unknownError), "unknown error hosts\n", unknownError, '\n')
 
+t2 = time.time()
+print("-" * 17 + "Time Used" + '-' * 17 + "\n" + str("Used %.2f" % (t2 - t1) + " seconds"))
 
 
