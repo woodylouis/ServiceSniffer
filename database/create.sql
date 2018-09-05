98b546c0ab2c0ec6bd1521a8464f4f70e1290d92
Li, Louis (L&W, Dutton Park) <Wenjin.Li@csiro.au>
2018-09-05 15:35:25 +1000

    the bot can store unqiue hosts and services

--- database/create.sql
+++ database/create.sql
@@ -8,28 +8,26 @@ id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 host_ip VARCHAR NOT NULL,
 port INTEGER NOT NULL,
 server_url VARCHAR NOT NULL
-
 );
+
 CREATE TABLE service (
 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-service_type VARCHAR NOT NULL
+service_type VARCHAR NOT NULL UNIQUE
 );
 
--- CREATE TABLE host_service (
--- host_id               INTEGER NOT NULL,
--- service_id            INTEGER NOT NULL,
---
--- PRIMARY KEY (host_id, service_id),
--- FOREIGN KEY (host_id) REFERENCES host(id),
--- FOREIGN KEY (service_id) REFERENCES service(id)
--- );
+CREATE TABLE host_service (
+host_id               INTEGER NOT NULL,
+service_id            INTEGER NOT NULL,
 
--- INSERT INTO service(id, name, service_type) VALUES (null, "odap", "OPENDAP");
--- INSERT INTO service(id, name, service_type) VALUES (null, "dap4", "DAP4" );
--- INSERT INTO service(id, name, service_type) VALUES (null, "http", "HTTPServer" );
--- INSERT INTO service(id, name, service_type) VALUES (null, "wcs" , "WCS");
--- INSERT INTO service(id, name, service_type) VALUES (null, "wms" , "WMS");
--- INSERT INTO service(id, name, service_type) VALUES (null, "ncss", "NetcdfSubset" );
--- INSERT INTO service(id, name, service_type) VALUES (null, "ncml", "NCML" );
--- INSERT INTO service(id, name, service_type) VALUES (null, "uddc", "UDDC" );
--- INSERT INTO service(id, name, service_type) VALUES (null, "iso" , "ISO");
+PRIMARY KEY (host_id, service_id),
+FOREIGN KEY (host_id) REFERENCES host(id),
+FOREIGN KEY (service_id) REFERENCES service(id)
+);
+
+INSERT INTO service(service_type) VALUES ("OPENDAP");
+INSERT INTO service(service_type) VALUES ("DAP4" );
+INSERT INTO service(service_type) VALUES ("HTTPServer" );
+INSERT INTO service(service_type) VALUES ("WCS");
+INSERT INTO service(service_type) VALUES ("WMS");
+INSERT INTO service(service_type) VALUES ("NetcdfSubset" );
+INSERT INTO service(service_type) VALUES ("NCML" );
