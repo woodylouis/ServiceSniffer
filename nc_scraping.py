9f4572f1ee413831e66e7eeff191ac6e560421e4
Li, Louis (L&W, Dutton Park) <Wenjin.Li@csiro.au>
2018-09-19 13:34:06 +1000

    now the bot can capture more dataset

--- nc_scraping.py
+++ nc_scraping.py
@@ -1,13 +1,10 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import re
-import random
 import requests
-from itertools import takewhile
 import store
 import tds_host
 from urllib.parse import urlsplit
 #from tds_host import hosts_services
+import helper
 
 hostFilesDict = {}
 
@@ -26,7 +23,7 @@ def getTDSInfo(host_services):
 """
 Get nc files if they are located at home page
 """
-def find_files_from_home_page(urls):
+def find_files_from_page(urls):
     #print(len(urls))
     urlTempList = []
     serviceTemp = {}
@@ -69,8 +66,6 @@ def find_files_from_home_page(urls):
 
 
     return (hostDatasetDict)
-    # print(hostDatasetDict)
-
 """
 Capture dataset in the dataset if they are located at home page
 """
@@ -164,15 +159,17 @@ def getHomeLink(urls):
     # print(listOfLinksInHomePage)
 
 """
-Get Iterate to find dataset in the CatalogueRef link
+Iterate to find in the CatalogueRef link in home page (from getHomeLink())
 """
-def getLinksOrFilesFromHomeDirectories(listOfLinks):
+def getFilesFromHomeDirectories(listOfLinks):
+    subURLs = []
+    subCatalogRefHrefList = []
+    links = []
     database = "C:\\Users\LI252\PycharmProjects\servicesniffer\database\database.sqlite"
     conn = store.create_connection(database)
-
     with conn:
         for link in listOfLinks:
-            # print(link)
+            #print(link)
             urlPortion = urlsplit(link, allow_fragments=True)
             hostIpWithPort = urlPortion.netloc
             hostIp = urlPortion.hostname
@@ -192,71 +189,114 @@ def getLinksOrFilesFromHomeDirectories(listOfLinks):
                             serviceName = service['name']
                             # print(serviceName)
                             serviceType = service['serviceType'.lower()]
+                            serviceTyp = helper.getURLPostfix(serviceType)
                             base = service['base']
                             #print(base)
-                            completeURL = f"http://{hostIpWithPort}{base}{ncUrlPath}"
-                            if serviceName == 'odap':
-                                serviceType = 'opendap'.upper()
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'dodsC':
-                                serviceType = 'opendap'.upper()
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'ncss':
-                                serviceType = 'NetcdfSubset'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'dap4':
-                                serviceType = 'DAP4'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'subsetServer':
-                                serviceType = 'NetcdfSubset'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'fileServer':
-                                serviceType = 'HTTPServer'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'http':
-                                serviceType = 'HTTPServer'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'wms':
-                                serviceType = 'WMS'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                #print(serviceId, hostId)
-                            elif serviceName == 'wcs':
-                                serviceType = 'WCS'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'ncml':
-                                serviceType = 'NCML'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                            elif serviceName == 'uddc':
-                                serviceType = 'UDDC'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                #print(serviceId, hostId)
-                            elif serviceName == 'iso':
-                                serviceType = 'ISO'
-                                serviceId = store.select_service_id_by_name(conn, serviceType)
-                                # print(serviceId, hostId)
-                        # for date in subDataset.find_all('date'):
-                        #     updateAt = date.text
+                            # print(base)
+                            if serviceTyp:
+                                completeURL = f"http://{hostIpWithPort}{base}{ncUrlPath}{serviceTyp}"
+                            else:
+                                completeURL = f"http://{hostIpWithPort}{base}{ncUrlPath}"
+                            serviceId = helper.compare(conn, serviceName)
 
                     datasetRecord = (ncName, description, completeURL, serviceId, hostId)
                     #print(datasetRecord)
-
                     existing = store.select_nc_name_and_description_and_url_path_and_service_id_and_host_id(conn, datasetRecord)
                     if datasetRecord != existing:
                         #print(ncRecord)
                         store.create_nc_files_record(conn, datasetRecord)
 
                     #print(urlPortion.hostname, subDataset['name'])
-        #print(serviceTemp)
-#
+
+        for link in listOfLinks:
+            postfix = 'catalog.xml'
+            host = urlsplit(link, allow_fragments=True)
+            # print(host)
+            hostIP = host.netloc
+            if postfix in link:
+                urlPrefix = link.strip(postfix)
+                # print(urlPrefix)
+            else:
+                urlPrefix = f"http://{hostIP}"
+
+            r = requests.get(link, timeout=0.5, allow_redirects=False)
+            soup = BeautifulSoup(r.text, features="lxml")
+            catalog = soup.find('catalog')
+            # print(catalog)
+            for catalogRef in catalog.find_all('catalogref'):
+                # print(catalogRef)
+                catalogRefHref = catalogRef['xlink:href']
+                # print(catalogRefHref)
+                # ls.append(catalogRefHref)
+                # print(urlPrefix, catalogRefHref)
+                completeURL = urlPrefix + catalogRefHref
+                subURLs.append(completeURL)
+
+        for subURL in subURLs:
+            host = urlsplit(subURL, allow_fragments=True)
+            hostIpWithPort = host.netloc
+            hostIp = host.hostname
+            hostId = store.select_host_id_by_host_ip(conn, hostIp)
+            try:
+                r = requests.get(subURL, timeout=0.5, allow_redirects=False)
+                soup = BeautifulSoup(r.text, features="lxml")
+                # print(r.url, r.status_code)
+                subCatalog = soup.find('catalog')
+                for subCatalogRef in subCatalog.find_all('catalogref'):
+                    subCatalogRefHref = subCatalogRef['xlink:href']
+                    ### We can get a larget list of sub URL here
+                    subCatalogRefHrefList.append(subCatalogRefHref)
+
+                for dataset in subCatalog.find_all('dataset'):
+                    if '.nc' in dataset['name']:
+                        datasetName = dataset['name']
+                        description = dataset['id']
+                        ncUrlPath = dataset['urlpath']
+                        for services in subCatalog.find_all('service'):
+                            for service in services.find_all('service'):
+                                serviceName = service['name']
+                                # print(serviceName)
+                                serviceType = service['serviceType'.lower()]
+                                serviceTyp = helper.getURLPostfix(serviceType)
+                                base = service['base']
+                                # print(base)
+                                if serviceTyp:
+                                    completeURL = f"http://{hostIpWithPort}{base}{ncUrlPath}{serviceTyp}"
+                                else:
+                                    completeURL = f"http://{hostIpWithPort}{base}{ncUrlPath}"
+
+                                serviceId = helper.compare(conn, serviceName)
+
+                        datasetRecord = (datasetName, description, completeURL, serviceId, hostId)
+                        # print(datasetRecord)
+
+                        existing = store.select_nc_name_and_description_and_url_path_and_service_id_and_host_id(conn, datasetRecord)
+                        if datasetRecord != existing:
+                            # print(ncRecord)
+                            store.create_nc_files_record(conn, datasetRecord)
+
+            except AttributeError:
+                pass
+
+            except requests.exceptions.HTTPError:
+                """An HTTP error occurred."""
+                pass
+
+            except requests.exceptions.ConnectionError:
+                """A Connection error occurred."""
+                pass
+            except requests.exceptions.Timeout:
+                """
+                The request timed out while trying to connect to the remote server.
+
+                Requests that produced this error are safe to retry.
+                """
+                pass
+            except requests.exceptions.RequestException:
+                """
+                There was an ambiguous exception that occurred while handling your request.
+                """
+                pass
 
 
 
@@ -265,11 +305,11 @@ if __name__ == '__main__':
     hosts_services = tds_host.get_services(hosts)
     ##unique URLs
     urls = getTDSInfo(hosts_services)
-    listOfFile = find_files_from_home_page(urls)
+    listOfFile = find_files_from_page(urls)
     catch_data_into_dataset_table(listOfFile)
     # get list of Catalogue link from Home page
     listOfLinks = getHomeLink(urls)
-    getLinksOrFilesFromHomeDirectories(listOfLinks)
+    getFilesFromHomeDirectories(listOfLinks)
 
 
 
